e784f628bcb1afbb0cb96f01f91f0f5fb04358f9
Rahul Roy <erahulroy123@gmail.com>
2025-04-24 18:02:51 +0530

    Update 1 find min max in array.py

--- ARRAY/1 find min max in array.py
+++ ARRAY/1 find min max in array.py
@@ -12,3 +12,20 @@ def find_max_min(arr):
 arr = [1, 4, 3, 2, 6, 5]
 max_val, min_val = find_max_min(arr)
 print(f"Maximum: {max_val}, Minimum: {min_val}")
+
+#Divide and conquer
+def find_max_min(arr, low, high):
+    if low == high:
+        return arr[low], arr[low]
+    if high == low + 1:
+        if arr[low] > arr[high]:
+            return arr[low], arr[high]
+        else:
+            return arr[high], arr[low]
+    mid = (low + high) // 2
+    left_max, left_min = find_max_min(arr, low, mid)
+    right_max, right_min = find_max_min(arr, mid + 1, high)
+    return max(left_max, right_max), min(left_min, right_min)
+arr = [1, 4, 3, 2, 6, 5]
+max_val, min_val = find_max_min(arr, 0, len(arr) - 1)
+print(f"Maximum: {max_val}, Minimum: {min_val}")
