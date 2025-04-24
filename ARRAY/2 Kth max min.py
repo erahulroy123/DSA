2af899f8174c180e88c467291a80bf7b8d47c695
Rahul Roy <erahulroy123@gmail.com>
2025-04-25 00:54:51 +0530

    Update 2 Kth max min.py

--- ARRAY/2 Kth max min.py
+++ ARRAY/2 Kth max min.py
@@ -1,1 +1,19 @@
+#selection sort
+def kth_min_max_manual_sort(arr, k):
+    n = len(arr)
+    if k < 1 or k > n:
+        return None, None
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    kth_min = arr[k - 1]
+    kth_max = arr[n - k]
+    return kth_min, kth_max
+arr = [7, 10, 4, 3, 20, 15]
+k = 3
+kth_min, kth_max = kth_min_max_manual_sort(arr, k)
 
+#
