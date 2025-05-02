01910402543a7d07faaa96cdd5aa9a87ed2cf5ec
Rahul Roy <erahulroy123@gmail.com>
2025-05-03 03:06:36 +0530

    Create 12 Kadane's Algo.py

---
+++ ARRAY/12 Kadane's Algo.py
@@ -0,0 +1,11 @@
+#naive
+def kadane(arr):
+    max_sum = float('-inf')
+    current_sum = 0
+    for num in arr:
+        current_sum += num
+        if current_sum > max_sum:
+            max_sum = current_sum
+        if current_sum < 0:
+            current_sum = 0
+    return max_sum
