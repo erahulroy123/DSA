962f23b7d9f2c9515ffb3d9ecb18df1cf818ae91
Rahul Roy <erahulroy123@gmail.com>
2025-05-03 02:49:28 +0530

    Update 8 Minimise the maximum difference between heights.py

--- ARRAY/8 Minimise the maximum difference between heights.py
+++ ARRAY/8 Minimise the maximum difference between heights.py
@@ -1,16 +1,22 @@
-#greedy
+#greedy (with bubble sort)
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 def minimize_max_difference(arr, k):
     n = len(arr)
-    if n == 1:
+    if n <= 1:
         return 0
-    arr.sort()
-    min_height = arr[0] + k
-    max_height = arr[-1] - k
+    bubble_sort(arr)
     result = arr[-1] - arr[0]
+    smallest = arr[0] + k
+    largest = arr[-1] - k
     for i in range(1, n):
         if arr[i] < k:
             continue
-        min_curr = min(min_height, arr[i] - k)
-        max_curr = max(max_height, arr[i - 1] + k)
+        min_curr = min(smallest, arr[i] - k)
+        max_curr = max(largest, arr[i - 1] + k)
         result = min(result, max_curr - min_curr)
     return result
